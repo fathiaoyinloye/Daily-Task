1fdab02c0226cccace4f0f978160c0f2d16be846
fathiaoyinloye <you@example.com>
2025-10-24 08:22:36 +0100

    ...

--- test_timmy_and_sarah_love.py
+++ test_timmy_and_sarah_love.py
@@ -1,9 +1,23 @@
 import unittest
-#from timmy_sarah import *
+from timmy_sarah_love import *
 
 class TestToUppercase(unittest.TestCase):
 
-	def test_if_timmy_picks_a_flower(self):
-		actual = generate_timmy_flower(45)
-		expected(0)
+	def test_if_timmy_and_sarah_picks_a_flower(self):
+		actual = is_true_love(45,6)
+		expected = True
+		self.assertEqual(actual,expected)
+
+	def test_if_the_right_response_is_given_if_one_picks_even_number_of_flowers(self):
+		actual = is_true_love(45,6)
+		expected = True
+		self.assertEqual(actual,expected)
+
+	def test_if_the_right_response_is_given_if_one_picks_old_number_of_flowers(self):
+		actual = is_true_love(5,6)
+		expected = True
+		self.assertEqual(actual,expected)
+	def test_if_the_right_response_is_given_if_both_picks_old_number_of_flowers(self):
+		actual = is_true_love(5,1)
+		expected = False
 		self.assertEqual(actual,expected)
